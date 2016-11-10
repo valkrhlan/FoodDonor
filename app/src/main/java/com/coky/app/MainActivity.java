9d4cbd5268a5490fae6584e1b915ea8b488522c7
Matija Čoklica <matcoklic@foi.hr>
2016-11-10 17:17:13 +0100

    Dodan gitignore.

--- app/src/main/java/com/coky/app/MainActivity.java
+++ app/src/main/java/com/coky/app/MainActivity.java
@@ -14,5 +14,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        //sad gitignore
     }
 }
