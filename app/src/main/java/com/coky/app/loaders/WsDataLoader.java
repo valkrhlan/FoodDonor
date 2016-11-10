fdad80ba46aa56fbfaaef06976a7ffd11aea3ca7
Matija Čoklica <matcoklic@foi.hr>
2016-11-10 13:50:01 +0100

    Nadograđen Gradle sa linijama koda koje definiraju ovisnost modula (app -> webservice, app -> core). Napravljen jedan dio WsDataLoader klase (pozivanje metoda iz FdWebServiceCaller klase i deklaracije Handlera)

--- app/src/main/java/com/coky/app/loaders/WsDataLoader.java
+++ app/src/main/java/com/coky/app/loaders/WsDataLoader.java
@@ -1,9 +1,46 @@
 package com.coky.app.loaders;
 
+import com.coky.webservice.FdWebServiceCaller;
+import com.coky.webservice.FdWebServiceHandler;
+
+import java.util.ArrayList;
+
 /**
  * Created by Čoky on 10.11.2016..
  */
 
 public class WsDataLoader {
+    public void prijava(ArrayList<String> data){
+        FdWebServiceCaller prijavaWs = new FdWebServiceCaller(prijavaHandler);
+        prijavaWs.CallWs("prijava",data);
+    }
+    public void registracijaFizicka(ArrayList<String> data){
+        FdWebServiceCaller regFizickaWs = new FdWebServiceCaller(regFizickaHandler);
+                regFizickaWs.CallWs("prijava",data);
+    }
+    public void registracijaPravna(ArrayList<String> data){
+        FdWebServiceCaller regPravnaWs = new FdWebServiceCaller(regPravnaHandler);
+                regPravnaWs.CallWs("prijava",data);
+    }
 
+    FdWebServiceHandler prijavaHandler = new FdWebServiceHandler() {
+        @Override
+        public void onDataArrived(String message, boolean ok) {
+
+        }
+    };
+
+    FdWebServiceHandler regFizickaHandler = new FdWebServiceHandler() {
+        @Override
+        public void onDataArrived(String message, boolean ok) {
+
+        }
+    };
+
+    FdWebServiceHandler regPravnaHandler = new FdWebServiceHandler() {
+        @Override
+        public void onDataArrived(String message, boolean ok) {
+
+        }
+    };
 }
