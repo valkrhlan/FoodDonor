057a5baa1e0bc3b4d8975bc6f4abd8eb17a80a24
Matija Čoklica <matcoklic@foi.hr>
2016-11-10 17:08:34 +0100

    Inicijalna verzija xml MainActivity. Priprema za gitignore

--- app/src/main/java/com/coky/app/MainActivity.java
+++ app/src/main/java/com/coky/app/MainActivity.java
@@ -2,6 +2,10 @@ package com.coky.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
