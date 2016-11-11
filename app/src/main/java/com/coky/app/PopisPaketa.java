cf46812169d944c12689276ef81444083393ae76
Matija Čoklica <matcoklic@foi.hr>
2016-11-11 13:58:55 +0100

    Napravljena klasa PopisPaketa (radi potreba implementacije odjave).

---
+++ app/src/main/java/com/coky/app/PopisPaketa.java
@@ -0,0 +1,13 @@
+package com.coky.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PopisPaketa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_popis_paketa);
+    }
+}
