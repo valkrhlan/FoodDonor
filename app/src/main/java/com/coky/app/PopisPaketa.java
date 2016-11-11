3ff673e5469df10d3fa12a37648dd4d05c000e61
Matija Čoklica <matcoklic@foi.hr>
2016-11-11 14:10:28 +0100

    Napravljen novi gumb (odjava) sa svojom funkcionalnošću (završetak rada u trenutnom activity-ju) u actionbar-u kod activity-a "PopisPaketa".

--- app/src/main/java/com/coky/app/PopisPaketa.java
+++ app/src/main/java/com/coky/app/PopisPaketa.java
@@ -1,7 +1,10 @@
 package com.coky.app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class PopisPaketa extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class PopisPaketa extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_popis_paketa);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.odjava) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
