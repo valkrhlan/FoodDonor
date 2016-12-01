160dca9a684b41198bdf84be26261e9d98b2e39a
Matija Čoklica <matcoklic@foi.hr>
2016-12-01 23:59:03 +0100

    Implementiran adapter i prikaz svakog paketa na popisu paketa. Paketi se mogu (u kodu) ručno dodavati/brisati, budući da su trenutni "mock" podaci.

--- FoodDonor/app/src/main/java/com/coky/app/adapters/PaketAdapter.java
+++ FoodDonor/app/src/main/java/com/coky/app/adapters/PaketAdapter.java
@@ -1,8 +1,52 @@
 package com.coky.app.adapters;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.coky.app.R;
+import com.coky.core.entities.Paket;
+
+import java.util.ArrayList;
+
 /**
  * Created by Čoky on 1.12.2016..
  */
 
-public class PaketAdapter {
+public class PaketAdapter extends ArrayAdapter<Paket> {
+
+    public static class ViewHolder{
+        TextView paketBroj;
+        TextView paketDatum;
+        TextView paketStatus;
+    }
+
+    public PaketAdapter(Context context, ArrayList<Paket> paketi){
+        super(context, 0, paketi);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent){
+        Paket paket = getItem(position);
+        ViewHolder viewHolder;
+        if(convertView == null){
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.paket_item, parent, false);
+            viewHolder = new ViewHolder();
+            viewHolder.paketBroj = (TextView) convertView.findViewById(R.id.paketBroj);
+            viewHolder.paketDatum = (TextView) convertView.findViewById(R.id.paketDatum);
+            viewHolder.paketStatus = (TextView) convertView.findViewById(R.id.paketStatus);
+            convertView.setTag(viewHolder);
+        }
+        else{
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+        viewHolder.paketBroj.setText(Long.toString(paket.getBroj()));
+        viewHolder.paketDatum.setText(paket.getDatum());
+        viewHolder.paketStatus.setText(paket.getStatus());
+
+        return convertView;
+    }
 }
--- FoodDonor/app/src/main/java/com/coky/app/fragments/DonorPopisPaketa.java
+++ FoodDonor/app/src/main/java/com/coky/app/fragments/DonorPopisPaketa.java
@@ -9,22 +9,42 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.coky.app.R;
+import com.coky.app.adapters.PaketAdapter;
+import com.coky.core.entities.Paket;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class DonorPopisPaketa extends ListFragment {
-
-    public DonorPopisPaketa() {
+    //-----Ovo neznam jel uopće potrebno, budući da bi moglo stvarati problema, zakomentiral sam
+    /*public DonorPopisPaketa() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_donor_popis_paketa, container, false);
+    }*/
+
+    private ArrayList<Paket> paketi;
+    private PaketAdapter paketAdapter;
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState){
+        super.onActivityCreated(savedInstanceState);
+        paketi = new ArrayList<Paket>();
+        Paket paket1 = new Paket(1,"1.12.2016","Dostupno");
+        Paket paket2 = new Paket(2,"29.11.2016","Preuzeto");
+        Paket paket3 = new Paket(1337, "11.9.2020.", "Dostupno");
+        paketi.add(paket1);
+        paketi.add(paket2);
+        paketi.add(paket3);
+        paketAdapter = new PaketAdapter(getActivity(), paketi);
+        setListAdapter(paketAdapter);
+        registerForContextMenu(getListView());
     }
-
-
 
 
 }
