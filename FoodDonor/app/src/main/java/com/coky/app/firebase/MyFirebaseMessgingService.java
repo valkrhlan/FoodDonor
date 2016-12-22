0e7a83f6ab83f990858031823a71ecd12ef60544
Matija Čoklica <matcoklic@foi.hr>
2016-12-22 13:16:39 +0100

    implementirana klasa MyFirebaseMessgingService - služi za rukovanje notifikacija koje Firebase šalje nakon zaprimljenog "Push"-a od strane našeg WS-a

--- FoodDonor/app/src/main/java/com/coky/app/firebase/MyFirebaseMessgingService.java
+++ FoodDonor/app/src/main/java/com/coky/app/firebase/MyFirebaseMessgingService.java
@@ -1,7 +1,14 @@
 package com.coky.app.firebase;
 
+import android.content.Intent;
+import android.util.Log;
+
+import com.coky.app.MainActivity;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by Valentina on 22.12.2016..
@@ -12,6 +19,32 @@ public class MyFirebaseMessgingService extends FirebaseMessagingService{
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        super.onMessageReceived(remoteMessage);
+        if (remoteMessage.getData().size() > 0) {
+            Log.e(TAG, "Data Payload: " + remoteMessage.getData().toString());
+            try {
+                JSONObject json = new JSONObject(remoteMessage.getData().toString());
+                sendPushNotification(json);
+            } catch (Exception e) {
+                Log.e(TAG, "Exception: " + e.getMessage());
+            }
+        }
     }
+
+    private void sendPushNotification(JSONObject json) {
+        Log.e(TAG, "Notification JSON " + json.toString());
+        try {
+            JSONObject data = json.getJSONObject("data");
+            String title = data.getString("title");
+            String message = data.getString("message");
+            String imageUrl = data.getString("image");
+            MyNotificationManager mNotificationManager = new MyNotificationManager(getApplicationContext());
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            mNotificationManager.showSmallNotification(title, message, intent);
+        } catch (JSONException e) {
+            Log.e(TAG, "Json Exception: " + e.getMessage());
+        } catch (Exception e) {
+            Log.e(TAG, "Exception: " + e.getMessage());
+        }
+    }
+
 }
