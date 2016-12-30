7c7d440a832879cec9e0ee7f2a046a0f60ca988b
valkrhlan <valkrhlan@foi.hr>
2016-12-30 22:57:19 +0100

    Dosta je za danas. Dodan OnClick Listener za gumb pohrani promjene (Meni ->notifikacije).

--- FoodDonor/app/src/main/java/com/coky/app/NotifikacijeOpcije.java
+++ FoodDonor/app/src/main/java/com/coky/app/NotifikacijeOpcije.java
@@ -1,30 +1,54 @@
 package com.coky.app;
 
+import android.content.Context;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.coky.app.R;
 import com.coky.app.klase.ItemNotifikacijaMoguceOpcije;
 import com.coky.app.klase.NotifikacijaMoguceOpcije;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class NotifikacijeOpcije extends AppCompatActivity {
 
-    private RadioGroup radioGroup;
+    private RadioGroup radioGroupInterval;
+    private RadioGroup notifOption;
+
+    @BindView(R.id.btnPohraniPromjeneNO)
+    Button btnPohraniPromijene;
+
+    @OnClick(R.id.btnPohraniPromjeneNO)
+    public void btnPohraniPromijeneNOClick(View view){
+        int idNotif=notifOption.getCheckedRadioButtonId();
+        if(idNotif==-1){
+            Toast.makeText(this,"Odaberite neku od mogućnosti!",Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this,"Ok je!",Toast.LENGTH_SHORT).show();
+
+        }
+      }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notifikacije_opcije);
+        ButterKnife.bind(this);
         final RadioButton konfOption = (RadioButton)findViewById(R.id.konfigurabilniOption);
-        //konfOption.setOnClickListener();
-        final RadioGroup radioGroupInterval = (RadioGroup)findViewById(R.id.radioGroupKonfigurabilnoIntervalNO);
+        radioGroupInterval = (RadioGroup)findViewById(R.id.radioGroupKonfigurabilnoIntervalNO);
         if(konfOption.isChecked()){
             radioGroupInterval.setVisibility(View.VISIBLE);
         }
-        RadioGroup notifOption = (RadioGroup)findViewById(R.id.radioGroupNotifikacijeNO);
+        notifOption = (RadioGroup)findViewById(R.id.radioGroupNotifikacijeNO);
         notifOption.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener(){
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
