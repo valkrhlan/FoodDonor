d0ee893a75fe319ffa15f65950b80fe3b5583f80
Aleksandar Trajkov <aletrajko@foi.hr>
2017-01-30 18:22:37 +0100

    Dodavanje gumba i nova aktivnost
    Acitivity za mapu i prikaz donora i volontera grafički

---
+++ FoodDonor/app/src/main/java/com/coky/app/MapaPaket.java
@@ -0,0 +1,13 @@
+package com.coky.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MapaPaket extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mapa_paket);
+    }
+}
